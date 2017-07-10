f91bb84a2b3d028a7896d5805e8635451c79893b
pedroliman <pedrolima.n@gmail.com>
2017-07-10 16:15:53 -0300

    V 0.0.2

--- app.R
+++ app.R
@@ -1,47 +1,124 @@
-# Bibliotecas que vou usar
+# Este é um app que possui a interface para a calculadora oshcba.
+## Se está rodando o app pela primeira vez, rode estes comando antes de executar o aplicativo: (Antes de rodar o comando, exclua o "#")
+# install.packages(c("shiny","ggplot2","readxl","mc2d","dplyr","devtools"))
+#Se precisar atualizaro app, rode este comando:
+# library(devtools)
+# install_github("pedroliman/oshcba")
 library(shiny)
-library(mc2d)
-library(ggplot2)
-library(lhs)
-library(readxl)
-library(dplyr)
 library(oshcba)
+library(ggplot2)
+library(readxl)
+library(mc2d)
+library(dplyr)
+library(reshape2)
+library(purrr)
 
-texto_de_ajuda = ("Carregue os dados de Input e o app irá calcular as suas Despesas com Absenteismo")
+### Definindo uma seed fixa fora do app para ter replicações mehor comparáveis.
+set.seed(1000)
 
-ui <- fluidPage(title = "CAlculadora em SST",
-  # Titulo
-  # titlePanel("Simulador Probabilistico - v0"),
-  navbarPage("Calculadora",
-             tabPanel("Calcuadora e Histograma",
-                      sidebarLayout(
-                        sidebarPanel(
-                          helpText(texto_de_ajuda),
-                          fileInput("Arquivo de Inputs",
-                                    inputId = "DadosInput")
-                        ),
-                        # Show a plot of the generated distribution
-                        mainPanel("Teste",
-                          plotOutput("histograma_absenteismo")
-                        )
-                      )
-             )
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Calculadora de Custos e Beneficios SST - V. 0.0.2"),
+  
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      "Faca Upload de seus dados de Input",
+      fileInput("Dados de Input",
+                inputId = "DadosInput",buttonLabel = "Arquivo.xlsx")
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      
+      tabsetPanel(
+        tabPanel("Graficos",
+                 selectInput("Iniciativa", "Selecione a Iniciativa para exibir os Graficos",
+                             c("Iniciativa1", "Iniciativa2", "Iniciativa3", "Iniciativa4", "Iniciatva5", "Iniciativa6", "Iniciativa7", "Iniciativa8", "Iniciativa9", "Iniciativa10", "TodasIniciativas")),
+                 plotOutput("histograma_absenteismo")
+                 ), 
+        tabPanel("Tabela de Resultados", tableOutput("table"), downloadButton('downloadData', 'Baixar Tabela de Resultados'))
+      )
+    )
   )
 )
 
-server <- function(input, output) {
-
+# Define server logic required to draw a histogram
+server <- function(input, output, session) {
+  
+  # Esta função apenas retorna o arquivo de Dados
+  CarregaDados <- reactive({
+    arquivodados <- input$DadosInput
+    if (is.null(arquivodados))
+      return(NULL)
+    file.copy(arquivodados$datapath,
+              paste(arquivodados$datapath, ".xlsx", sep=""))
+    return(arquivodados)
+  })
+  
+  # Esta função retorna a lista inputs
+  inputs = reactive({
+    arquivoinputs = CarregaDados()
+    if (is.null(arquivoinputs))
+      return(NULL)
+    inputs = carregar_inputs(arquivoinputs)
+    return(carregar_inputs(inputs))
+  })
+  
   # Dados de Absenteismo simulados
   dados_simulados = reactive(
     {
-      dados = simular_lhs(input$DadosInput)
+      inputs = CarregaDados()
+      if (is.null(inputs))
+        return(NULL)
+      dados = simular_temp_absenteismo(paste(inputs$datapath, ".xlsx", sep=""))
       return(dados)
     })
-
-  output$histograma_absenteismo <- renderPlot({
-    qplot(dados,geom = "histogram",
-          main="Histograma de Despesas em Absenteismo")
+  
+  
+  # Iniciativas Simuladas: Este código provavelmente deveria ser implementado fora do layout
+  iniciativas = reactive({
+    inputs = inputs()
+    if (is.null(inputs))
+      return(NULL)
+    iniciativas = obter_cenarios(inputs) %>%
+      filter(!CenarioASIS) %>%
+      select(Cenario)
+    iniciativas = as.vector(t(iniciativas))
+    return(iniciativas)
   })
+  
+  output$histograma_absenteismo <- renderPlot({
+    dados_simulados = dados_simulados()
+    if (!is.null(dados_simulados))
+      dados_simulados = dados_simulados %>% filter(Cenario.y == input$Iniciativa) %>% select(Cenario.y, BeneficioAbsenteismo, BeneficioTotalCBR, RazaoBeneficioCusto)
+
+    ggplot(data = melt(dados_simulados), mapping = aes(x = value)) + 
+      geom_histogram(bins = 15) + facet_wrap(~variable, scales = 'free_x')
+    
+    # qplot(dados_simulados$RazaoBeneficioCusto,geom = "histogram",
+    #       main="Histograma de Despesas em Absenteismo")
+  })
+  
+  output$table <- renderTable({
+    head(dados_simulados(),n = 100)
+  })
+  
+  output$completetable <- renderTable({
+    dados_simulados()
+  })
+  
+  output$downloadData <- downloadHandler(
+    filename = function() { paste("output_simulacao", '.csv', sep='') },
+    content = function(file) {
+      #write.csv(datasetInput(), file)
+      write.table(dados_simulados(),file,sep=";",dec=",",row.names = FALSE)
+    }
+  )
+  
 }
 
+# Run the application
 shinyApp(ui = ui, server = server)
--- teste.R
+++ teste.R
@@ -1,4 +1,30 @@
 
+library(shiny)
+library(mc2d)
+library(ggplot2)
+library(lhs)
+library(readxl)
+library(dplyr)
+library(oshcba)
+library(reshape2)
+library(purrr)
 
-parametros = obter_parametros(inputs)
-browser()
+simulado = simular_temp_absenteismo()
+
+variaveis = c("RazaoBeneficioCusto", "BeneficioTotal", "BeneficioAbsenteismo", "CustoTotalCBR")
+
+dados_grafico = simulado %>% select(Cenario.y, BeneficioAbsenteismo, BeneficioTotalCBR, RazaoBeneficioCusto) %>% filter(Cenario.y == "Iniciativa2")
+
+
+# Nada do que está aqui em baixo funcionou. Preciso estudar o GGPLOT para fazer estas coisas:
+ggplot(data = melt(dados_grafico), mapping = aes(x = value)) + 
+  geom_histogram(bins = 15) + facet_wrap(~variable, scales = 'free_x')
+
+
+ggplot(data = melt(dados_grafico), mapping = aes(x = value)) + 
+  geom_histogram(bins = 15) + facet_grid(. ~ Cenario.y)
+
+
+p <- ggplot(mpg, aes(displ, cty)) + geom_point()
+
+p + facet_grid(. ~ cyl)
